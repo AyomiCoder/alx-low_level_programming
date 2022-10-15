5fa59d55f3b2bada36fc09a966a400458a520acc
Aluko Ayomide  Stephen <64385390+AyomiCoder@users.noreply.github.com>
2022-10-15 18:52:13 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-	      char ch = 'a';
+	char ch = 'a';
 
-	      while (ch <= 'z')
-	      {
-		          if (ch != 'e' && ch != 'q')
-		          {
-			                putchar(ch);
-		           }
-		          ch++;
-	      }
-	      putchar('\n');
-	      return (0);
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
 }
