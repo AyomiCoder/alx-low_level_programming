2a0ca8b88ef81d54d1e67c1680aff19a1683f616
Aluko Ayomide  Stephen <64385390+AyomiCoder@users.noreply.github.com>
2022-11-05 16:18:44 +0000

    Update 2-strlen_recursion.c

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,17 @@
 #include "main.h"
-
 /**
- * _strlen_recursion - returns the length of a string
- * @s: string to calculate the length of
- *
- * Return: length of the string
+ *_strlen_recursion - length of a string
+ *@s: pointer block of memory to fill
+ *Return: strlen_recursion
  */
+
 int _strlen_recursion(char *s)
 {
-	if (*s == '\0')
-		return (0);
-	return (1 + _strlen_recursion(s + 1));
+
+/*Base condition*/
+if (*s == '\0')
+return (0);
+
+else
+return (1 + _strlen_recursion(s + 1)); /*Sum 1*/
 }
