9b2b9f7c812ec9b18fcd3b04e6a78105330ae16d
AyomiCoder <alukoayomide23@gmail.com>
2022-11-17 11:25:14 +0000

    variadic func and func pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * print_name - prints a name
+ * @name: string
+ * @f: function pointer
+ * Return: void
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != NULL && name != NULL)
+		f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+Readme for this current foldre on function pointers
+
